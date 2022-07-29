24ad58e7f02e331ab56b95500dd42dbd49dbe337
aniketnangare23 <aniketnangare23@gmail.com>
2022-07-29 18:10:38 +0530

    Forget Password

--- admin/forgetpassword.php
+++ admin/forgetpassword.php
@@ -1,3 +1,95 @@
+<?php
+include("../assets/include/config.php");
+if(isset($_POST['forget'])){
+  $Email=$_POST['email'];
+
+$Password= rand(100000, 999999);
+$hasPassword=password_hash($Password,PASSWORD_BCRYPT);
+
+
+$from = 'Enquiry <naiduvedant@gmail.com>' . "\r\n";
+$sendTo = 'Enquiry <'.$Email.'>';
+$subject = 'Your New Password';
+// $fields = array( 'name' => 'name' );
+$from = 'Tectignis IT Solution: 1.0' . "\r\n";
+$from .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+
+$emailText = '
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="x-apple-disable-message-reformatting"> 
+  <title></title>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
+  <style>
+      html,
+body {
+  margin: 0 auto !important;
+  padding: 0 !important;
+  height: 100% !important;
+  width: 100% !important;
+  background: #f1f1f1;
+}
+* {
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+}
+  </style>
+</head>
+<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f1f1f1;">
+<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+<div style="margin:50px auto;width:70%;padding:20px 0">
+<div style="border-bottom:1px solid #eee">
+  <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Agreerent</a>
+</div>
+<p>Please enter below password</p>
+<h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">'.$Password.'</h2>
+<p style="font-size:0.9em;">Regards,<br />Your Brand</p>
+<hr style="border:none;border-top:1px solid #eee" />
+<div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+  <p>Your Brand Inc</p>
+  <p>1600 Amphitheatre Parkway</p>
+  <p>California</p>
+</div>
+</div>
+</div>
+</body>
+</html>';
+
+try{
+foreach($_POST as $key => $value){
+  if(isset($fields[$key])){
+    $emailText.="$fields[$key]: $value\n";
+  }
+}
+if( mail($sendTo,$subject,$emailText, "From:" .$from)){
+
+    $sql=mysqli_query($conn,"UPDATE `admin_login` SET `password`='$hasPassword' WHERE email='$Email'");
+
+    if($sql==1){
+        echo '<script>alert("data successfully submitted");</script>';
+        header("location:clientlogin.php");
+    }else {
+        echo '<script>alert("oops...somthing went wrong");</script>';
+    }
+ 
+}else{
+  echo "eeee $sendTo $subject $emailText $from";
+}
+}
+catch(\Exception $e){
+echo "not done";
+}
+
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -63,18 +155,18 @@ border-bottom-right-radius: .3rem;
                     <!-- <h6>SACHIN ENTERPRISE</h6> -->
                 </div><br>
 
-                <form>
+                <form method="POST">
                   <p align="center">Forget Password</p><br>
 
                   <div class="form-outline mb-4">
                     <input type="email" id="form2Example11" class="form-control"
-                      placeholder="Enter Email address" />
+                      placeholder="Enter Email address" name="email" />
                     <label class="form-label" for="form2Example11">Email</label>
                   </div>
 
 
                   <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-block fa-lg  mb-3" type="submit" style="background:#4ab242; color:white;">Submit</button>
+                    <button class="btn btn-block fa-lg  mb-3" name="forget"type="submit" style="background:#4ab242; color:white;">Submit</button>
                     <a class="text-muted" href="adminlogin.php">Login Your Account</a>
                   </div>
 
