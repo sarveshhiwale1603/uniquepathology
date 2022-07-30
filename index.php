89efaa71a56cafbd3447a613d4d5df46b5494dbe
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-07-30 16:05:16 +0530

    s

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ include("assets/include/config.php");
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item">
-        <img class="d-block w-100 imgCarousle" src="admin/dist/img/gallery/<?php echo $arr['gallery_img'] ?>" alt="First slide"  >
+        <img class="d-block w-100 imgCarousle" style=" opacity: .5;"  src="admin/dist/img/gallery/<?php echo $arr['gallery_img'] ?>" alt="First slide" width="1000" height="586">
         <div class="carousel-caption  ">
           <h1><?php echo $arr['banner_heading'] ?></h1>
           <!-- <h1>We Provide Best Solution.</h1> -->
@@ -103,8 +103,8 @@ include("assets/include/config.php");
         </div>
       </div>
       <div class="carousel-item active">
-        <img class="d-block w-100 imgCarousle" src="admin/dist/img/gallery/<?php echo $arr1['gallery_img'] ?>" alt="Second slide"  >
-        <div class="carousel-caption ">
+        <img class="d-block w-100 imgCarousle" style=" opacity: .5;" src="admin/dist/img/gallery/<?php echo $arr1['gallery_img'] ?>" alt="Second slide" width="1000" height="586">
+        <div class="carousel-caption  ">
           <h1><?php echo $arr1['banner_heading'] ?></h1>
           <!-- <h1>Welcome To Unique Diagnostics & Pathology Laboratory</h1> -->
           <h2><?php echo $arr1['banner_content'] ?></h2>
@@ -112,8 +112,8 @@ include("assets/include/config.php");
         </div>
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100 imgCarousle" src="admin/dist/img/gallery/<?php echo $arr2['gallery_img'] ?>" alt="Third slide"  >
-        <div class="carousel-caption ">
+        <img class="d-block w-100 imgCarousle" style=" opacity: .5;"  src="admin/dist/img/gallery/<?php echo $arr2['gallery_img'] ?>" alt="Third slide" width="1000" height="586">
+        <div class="carousel-caption  ">
           <h1><?php echo $arr2['banner_heading'] ?></h1>
           <!-- <h1>We Offering High-Quality Accurate Tests At Affordable Prices.</h1> -->
           <h2><?php echo $arr2['banner_content'] ?></h2>
