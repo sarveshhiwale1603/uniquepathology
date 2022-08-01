c77f591b50bb625d8b4553b2af8cc22a8a568cb4
aniketnangare23 <aniketnangare23@gmail.com>
2022-08-01 15:16:18 +0530

    s

--- index.php
+++ index.php
@@ -416,7 +416,8 @@ include("assets/include/config.php");
                 <div class="validate"></div>
               </div>
               <div class="col-md-6 form-group mt-3">
-                <input type="text" onfocus="(this.type='date')" name="date" class="form-control " required id=""
+              <!-- onfocus="(this.type='date')" -->
+                <input type="date"  name="date" class="form-control " required id=""
                   placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
                 <div class="validate"></div>
               </div>
