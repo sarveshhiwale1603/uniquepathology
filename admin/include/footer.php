0fcd6162012b98daf6570c490d38d8a42c7b6b4b
aniketnangare23 <aniketnangare23@gmail.com>
2022-08-01 09:38:28 +0530

    ch

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -7,7 +7,7 @@
     </footer> -->
     <footer class="main-footer">
       <div class="row">
-        <div class="col-md-6 text-end">
+        <div class="col-md-6 text-right">
         <strong>Copyright &copy; 2022. Unique Pathology</strong>
         </div>
         <div class="col-md-6">
