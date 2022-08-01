20863d3cbcb22a87b093077e387c288e03bf91ee
aniketnangare23 <aniketnangare23@gmail.com>
2022-08-01 09:35:34 +0530

    ch

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,7 +1,17 @@
-<footer class="main-footer">
+<!-- <footer class="main-footer">
       <div class="float-right d-none d-sm-block">
         Developed by <a href="http://tectignis.in" target="_blank" style="color:#869099 !important"> <b>Tectignis IT Solutions</b></a>
       </div>
       <strong>Copyright &copy; 2022. Unique Pathology</strong> 
 
-    </footer>
+    </footer> -->
+    <footer class="main-footer">
+      <div class="row">
+        <div class="col-md-6">
+        <strong>Copyright &copy; 2022. Unique Pathology</strong>
+        </div>
+        <div class="col-md-6">
+        Developed by <a href="http://tectignis.in" target="_blank" style="color:#869099 !important"> <b>Tectignis IT Solutions</b></a>
+        </div>
+      </div>
+</footer>
