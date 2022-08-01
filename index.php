edcffd8140f9482103594bdb2cf87c7e5b424860
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-08-01 17:19:35 +0530

    ss

--- index.php
+++ index.php
@@ -511,9 +511,9 @@ include("assets/include/config.php");
         $name=$_POST['name'];
         $email=$_POST['email'];
         $phone=$_POST['phone'];
-        $date=$_POST['date'];
+        // $date=$_POST['date'];
     
-        $sql=mysqli_query($conn, "INSERT INTO `make_an_appointment`(`name`,`email`,`phone`,`date`) VALUES('$name','$email','$phone','$date')");
+        $sql=mysqli_query($conn, "INSERT INTO `make_an_appointment`(`name`,`email`,`phone`) VALUES('$name','$email','$phone')");
     
         if( $sql==1){
         // echo "<script>window.location='index.php';</script>";
