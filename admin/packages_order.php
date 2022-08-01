732859f414a2d8b44d2eb71c32cf00e24c8090e3
aniketnangare23 <aniketnangare23@gmail.com>
2022-08-01 09:46:21 +0530

    preloader

--- admin/packages_order.php
+++ admin/packages_order.php
@@ -35,8 +35,8 @@ if(isset($_GET['gen'])){
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
         <!-- Preloader  -->
-<div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+      <div class="preloader flex-column justify-content-center align-items-center">
+          <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
     </div>
   <!-- Navbar -->
   <?php include("include/header.php"); ?>
