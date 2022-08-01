013f372a3fe72d586a01b296bedf32d4644ccc34
aniketnangare23 <aniketnangare23@gmail.com>
2022-08-01 09:23:14 +0530

    ch

--- contact_us.php
+++ contact_us.php
@@ -109,7 +109,7 @@ include("assets/include/config.php");
                 <div class="col-md-6 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
                 </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
+                <div class="col-md-6 form-group mt-3">
                   <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
                 </div>
               </div>
