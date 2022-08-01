5e4f125573b2c790cc2701fc7dbf335419a3ec48
aniketnangare23 <aniketnangare23@gmail.com>
2022-08-01 09:22:27 +0530

    ch

--- contact_us.php
+++ contact_us.php
@@ -114,11 +114,11 @@ include("assets/include/config.php");
                 </div>
               </div>
               <div class="row">
-                <div class=" col-md-6 col-12">
+                <div class=" col-md-6 col-12 form-group">
                   <input type="tel" class="form-control" name="phone" id="phone" placeholder="Phone Number"
                     minlength="10" maxlength="10" required>
                 </div>
-                <div class="col-md-6 col-12 mt-3">
+                <div class="col-md-6 col-12 form-group mt-3">
                   <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
                 </div>
               </div>
